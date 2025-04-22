f5e98771651f23c958c39b24a568078d6683d2e9
abbystvns <abby.e.stevens@gmail.com>
2025-04-22 09:39:51 -0700

    increased reps

--- emews/R/exp_design.R
+++ emews/R/exp_design.R
@@ -1,20 +1,15 @@
 exp_design <- list(
-  sim_budget = 5000, # Total simulation budget
-  init_npar = 100,    # Number of initial design points
-  nrep = 10,         # Number of replications
-  grid_npar = 1000,    # Number of candidate points for grid
+  sim_budget = 3000, # Total simulation budget
+  init_npar = 20,    # Number of initial design points
+  nrep = 30,         # Number of replications
+  grid_npar = 100,    # Number of candidate points for grid
   nTS_samp = 100,    # Number of Thompson samples
   objective = "both", # one of (hosp, deaths, both)
   err_sig = 0.5,
   prop_sig = 0.3,
   ref = -2,
-  param_names = c('infected_count', 
-                  'susceptible_to_exposed_probability', 
-                  'seasonality_multiplier',
-                  'shielding_scaling', 
-                  'isolate_infectivity_household',
-                  'isolate_infectivity_nursinghome', 
-                  'initial_exposure_tick',
-                  'stay_at_home_probability', 
-                  'stoe_behavioral_adjustment_probability')
+  param_names =  c(
+    "susceptible.to.exposed.probability", 
+    "stay.at.home.probability", 
+    "stoe.behavioral.adjustment.probability")
 )

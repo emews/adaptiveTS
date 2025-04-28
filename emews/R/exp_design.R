972180e072b394c69f16bd76d493100ab6a4b8dd
abbystvns <abby.e.stevens@gmail.com>
2025-04-28 13:16:01 -0500

    time tracking, 4 params

--- emews/R/exp_design.R
+++ emews/R/exp_design.R
@@ -1,19 +1,14 @@
 exp_design <- list(
-  sim_budget = 5000, # Total simulation budget
-  init_npar = 100,    # Number of initial design points
-  nrep = 10,         # Number of replications
-  grid_npar = 1000,    # Number of candidate points for grid
+  sim_budget = 3000, # Total simulation budget
+  init_npar = 30,    # Number of initial design points
+  nrep = 30,         # Number of replications
+  grid_npar = 100,    # Number of candidate points for grid
   nTS_samp = 100,    # Number of Thompson samples
   objective = "both", # one of (hosp, deaths, both)
   err_sig = 0.5,
   prop_sig = 0.3,
   ref = -2,
-  param_names = c('infected.count', 
-                  'susceptible.to.exposed.probability', 
-                  'seasonality.multiplier',
-                  'shielding.scaling', 
-                  'isolate.infectivity.household',
-                  'isolate.infectivity.nursinghome', 
+  param_names = c('susceptible.to.exposed.probability', 
                   'initial.exposure.tick',
                   'stay.at.home.probability', 
                   'stoe.behavioral.adjustment.probability')
